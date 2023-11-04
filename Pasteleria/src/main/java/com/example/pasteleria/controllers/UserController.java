fb2c288c472a06318199278e3b812e28843d2e70
Drach <88101742+Drach23@users.noreply.github.com>
2023-11-03 22:30:52 -0600

    Archivo con los get/set/request mapping, autowired,rest controller

---
+++ Pasteleria/src/main/java/com/example/pasteleria/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.example.pasteleria.controllers;
+
+import com.example.pasteleria.Models.UserModel;
+import com.example.pasteleria.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    UserServices userService;
+
+    //get
+    @GetMapping()
+    public ArrayList<UserModel> findAllUsers(){
+        return userService.findAllUsers();
+    }
+
+    //post
+    @PostMapping()
+    public UserModel saveUser(@RequestBody UserModel user){
+        return userService.saveUser(user);
+    }
+}
