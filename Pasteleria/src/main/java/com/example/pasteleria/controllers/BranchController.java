90f97741b18ff1a53d5ed7ebe7117c087512eabc
Drach <88101742+Drach23@users.noreply.github.com>
2023-11-04 00:02:35 -0600

    Agregado del controlador de sucursales

--- Pasteleria/src/main/java/com/example/pasteleria/controllers/BranchController.java
+++ Pasteleria/src/main/java/com/example/pasteleria/controllers/BranchController.java
@@ -1,2 +1,29 @@
-package com.example.pasteleria.controllers;public class BranchController {
+package com.example.pasteleria.controllers;
+
+import com.example.pasteleria.Models.BranchModel;
+import com.example.pasteleria.services.BranchServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/branches")
+
+public class BranchController {
+    @Autowired
+    BranchServices branchServices;
+
+    //get
+    @GetMapping()
+    public ArrayList<BranchModel> findAllBranches(){
+        return branchServices.findAllBranches();
+    }
+    //post
+    @PostMapping
+    public BranchModel saveBranch(@RequestBody BranchModel branch){
+        return branchServices.saveBranch(branch);
+    }
+
+
 }
