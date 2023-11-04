c095eb6b501700a693441d3af8f85756f9750681
Drach <88101742+Drach23@users.noreply.github.com>
2023-11-04 00:04:37 -0600

    Agregacion de repositories para sucursales

--- Pasteleria/src/main/java/com/example/pasteleria/repositories/BranchRepository.java
+++ Pasteleria/src/main/java/com/example/pasteleria/repositories/BranchRepository.java
@@ -1,2 +1,10 @@
-package com.example.pasteleria.repositories;public interface BranchRepository {
+package com.example.pasteleria.repositories;
+
+import com.example.pasteleria.Models.BranchModel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BranchRepository extends CrudRepository<BranchModel,Integer> {
+
 }
