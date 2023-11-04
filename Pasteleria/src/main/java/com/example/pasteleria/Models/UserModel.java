b20dc01865b06f0a5009b879a4293a4d5d2c7ce3
Drach <88101742+Drach23@users.noreply.github.com>
2023-11-03 21:20:40 -0600

    Cambios para Backend

---
+++ Pasteleria/src/main/java/com/example/pasteleria/Models/UserModel.java
@@ -0,0 +1,58 @@
+package com.example.pasteleria.Models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false,unique = true)
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    //setters y getters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
