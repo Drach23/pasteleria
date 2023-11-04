69613ff8b41abaec963a580dbc9b74bcb9c6eefd
Drach <88101742+Drach23@users.noreply.github.com>
2023-11-03 22:32:39 -0600

    findAll and saveUser agregados

--- Pasteleria/src/main/java/com/example/pasteleria/services/UserServices.java
+++ Pasteleria/src/main/java/com/example/pasteleria/services/UserServices.java
@@ -1,2 +1,23 @@
-package com.example.pasteleria.services;public class UserServices {
+package com.example.pasteleria.services;
+
+import com.example.pasteleria.Models.UserModel;
+import com.example.pasteleria.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserServices {
+    @Autowired
+    UserRepository userRepository;
+    //obtener todos los usuarios registrados
+    public ArrayList<UserModel> findAllUsers(){
+        return (ArrayList<UserModel>)userRepository.findAll();
+    }
+    //Guardar un usuario
+
+    public UserModel  saveUser(UserModel user){
+        return userRepository.save(user);
+    }
 }
