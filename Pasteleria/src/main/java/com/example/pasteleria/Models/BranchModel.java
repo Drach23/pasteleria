018f2b0a9e081a63d1634c9bd4c73bd573ff9c26
Drach <88101742+Drach23@users.noreply.github.com>
2023-11-04 00:01:34 -0600

    Agregacion del modelo para sucursales

--- Pasteleria/src/main/java/com/example/pasteleria/Models/BranchModel.java
+++ Pasteleria/src/main/java/com/example/pasteleria/Models/BranchModel.java
@@ -1,2 +1,57 @@
-package com.example.pasteleria.Models;public class BranchModel {
+package com.example.pasteleria.Models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "sucursales")
+public class BranchModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false,unique = true)
+    private Integer id;
+    private String nombre;
+    private String email;
+    private String telefono;
+    private String estado;
+
+    //setter y getters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
