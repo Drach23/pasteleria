1efb421eaced7600c0d1db9e022c2a2780654014
Drach <88101742+Drach23@users.noreply.github.com>
2023-11-08 22:51:13 -0600

    Correccion de setter y getter en modelos

--- Pasteleria/src/main/java/com/example/pasteleria/controllers/ReviewController.java
+++ Pasteleria/src/main/java/com/example/pasteleria/controllers/ReviewController.java
@@ -1,1 +1,27 @@
-git add
+package com.example.pasteleria.controllers;
+
+import com.example.pasteleria.Models.ReviewModel;
+import com.example.pasteleria.services.ReviewServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/reviews")
+public class ReviewController {
+    @Autowired
+    ReviewServices reviewService;
+    //get
+    @GetMapping()
+    public ArrayList<ReviewModel> findAllReviews(){
+        return reviewService.findAllReviews();
+    }
+    //post
+    @PostMapping()
+    public ReviewModel saveReview(@RequestBody ReviewModel review){
+        return reviewService.saveReview(review);
+    }
+}
+
+
