3b595afc45006b9cfb51aef8645c64f452c7be58
Drach <88101742+Drach23@users.noreply.github.com>
2023-11-22 00:09:10 -0600

    agregado de findById

--- Pasteleria/src/main/java/com/example/pasteleria/services/ReviewServices.java
+++ Pasteleria/src/main/java/com/example/pasteleria/services/ReviewServices.java
@@ -1,11 +1,13 @@
 package com.example.pasteleria.services;
 
 import com.example.pasteleria.Models.ReviewModel;
+import com.example.pasteleria.Models.UserModel;
 import com.example.pasteleria.repositories.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class ReviewServices {
@@ -19,6 +21,9 @@ public class ReviewServices {
     //Guardar una nueva review
     public ReviewModel saveReview(ReviewModel review){
         return reviewRepository.save(review);
+    }
+    public Optional<ReviewModel> findReviewById(ReviewModel.ReviewModelId reviewModelId) {
+        return this.reviewRepository.findById(reviewModelId);
     }
 }
 
