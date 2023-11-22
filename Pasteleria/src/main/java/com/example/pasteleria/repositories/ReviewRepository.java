79db19f025469c65ea7d025a2996642387104877
Drach <88101742+Drach23@users.noreply.github.com>
2023-11-22 00:07:10 -0600

    Modificacion del repository a objeto y findById

--- Pasteleria/src/main/java/com/example/pasteleria/repositories/ReviewRepository.java
+++ Pasteleria/src/main/java/com/example/pasteleria/repositories/ReviewRepository.java
@@ -4,8 +4,11 @@ import com.example.pasteleria.Models.ReviewModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ReviewRepository extends CrudRepository<ReviewModel,Long> {
+import java.util.Optional;
 
+@Repository
+public interface ReviewRepository extends CrudRepository<ReviewModel,ReviewModel.ReviewModelId> {
+    Optional<ReviewModel> findById(ReviewModel.ReviewModelId reviewModelId);
 }
+
 
