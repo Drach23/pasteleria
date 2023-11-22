302b3553c76f8d353f7cb44b522ea8f62a0f3b9d
Drach <88101742+Drach23@users.noreply.github.com>
2023-11-22 09:46:12 -0600

    add findById for review

--- Pasteleria/src/main/java/com/example/pasteleria/repositories/ReviewRepository.java
+++ Pasteleria/src/main/java/com/example/pasteleria/repositories/ReviewRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface ReviewRepository extends CrudRepository<ReviewModel,ReviewModel.ReviewModelId> {
     Optional<ReviewModel> findById(ReviewModel.ReviewModelId reviewModelId);
+
 }
