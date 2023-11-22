b24db7554ec68ca8f1fdeb2b7e4115f85ecaabb3
Drach <88101742+Drach23@users.noreply.github.com>
2023-11-22 00:03:41 -0600

    agregado de update y findById con doble request param

--- Pasteleria/src/main/java/com/example/pasteleria/controllers/ReviewController.java
+++ Pasteleria/src/main/java/com/example/pasteleria/controllers/ReviewController.java
@@ -1,11 +1,14 @@
 package com.example.pasteleria.controllers;
 
 import com.example.pasteleria.Models.ReviewModel;
+
+import com.example.pasteleria.Models.UserModel;
 import com.example.pasteleria.services.ReviewServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/reviews")
@@ -22,6 +25,22 @@ public class ReviewController {
     public ReviewModel saveReview(@RequestBody ReviewModel review){
         return reviewService.saveReview(review);
     }
-}
+    @PutMapping()
+    public ReviewModel updateReview(@RequestBody ReviewModel review){
+        return reviewService.saveReview(review);
+    }
+
+    @GetMapping(path = "/findById")
+    public Optional<ReviewModel> findReviewById(
+            @RequestParam("userId") Long userId,
+            @RequestParam("productId") Long productId
+    ) {
+        ReviewModel.ReviewModelId reviewModelId = new ReviewModel.ReviewModelId();
+        reviewModelId.setUserId(userId);
+        reviewModelId.setProductId(productId);
+
+        return this.reviewService.findReviewById(reviewModelId);
+    }
+}git
 
 
