aec059aced812b41527b25afb1ee64f55e7adad9
Drach <88101742+Drach23@users.noreply.github.com>
2023-11-24 17:05:10 -0600

    add findById and editById

--- Pasteleria/src/main/java/com/example/pasteleria/controllers/ProductController.java
+++ Pasteleria/src/main/java/com/example/pasteleria/controllers/ProductController.java
@@ -1,11 +1,13 @@
 package com.example.pasteleria.controllers;
 
 import com.example.pasteleria.Models.ProductModel;
+import com.example.pasteleria.Models.UserModel;
 import com.example.pasteleria.services.ProductServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/productos")
@@ -21,6 +23,14 @@ public class ProductController {
     @PostMapping()
     public ProductModel saveUser(@RequestBody  ProductModel product){
         return productServices.saveProduct(product);
+    }
+    @GetMapping (path = "/findById")
+    public Optional<ProductModel> findProductById(@RequestParam("id")Long id){
+        return this.productServices.findProductById(id);
+    }
+    @DeleteMapping(path = "/deleteById")
+    public boolean deleteProduct(@RequestParam("id")Long id){
+        return this.productServices.deleteProduct(id);
     }
 
 
