906a433215e9d99ba9112c3d657a3b0eafb53f5b
Drach <88101742+Drach23@users.noreply.github.com>
2023-12-05 00:21:08 -0600

    modificacion de validacion de datos en el js

--- Pasteleria/src/main/resources/static/js/register.js
+++ Pasteleria/src/main/resources/static/js/register.js
@@ -1,84 +1,93 @@
 function registerSuccessMessage() {
-    const successDiv = document.getElementById("successMessage");
-    successDiv.innerText = "Registro exitoso.";
-    successDiv.classList.add("success-message");
-
-    // Eliminar el mensaje de éxito después de 5 segundos
-    setTimeout(function () {
-        successDiv.innerText = "";
-        successDiv.classList.remove("success-message");
-    }, 5000);
-    
-    window.location.href = "../html/loged.html";
-}
-
-function validateForm(){
-    let name = document.getElementById("inputName").value;
-    let lastName = document.getElementById("inputLastName").value;
-    let phone = document.getElementById("inputPhone").value;
-    let email = document.getElementById("inputEmail").value;
-    let password = document.getElementById("inputPassword").value;
-
-    if(name === "" || lastName === "" || phone === "" || email === "" || password === ""){
-        // Al menos un campo está vacío
-        const errorDiv = document.getElementById("errorMessage");
-        errorDiv.innerText = "Por favor, complete todos los campos.";
-        errorDiv.classList.add("error-message");
-
-        // Eliminar el mensaje de error después de 5 segundos
-        setTimeout(function () {
-            errorDiv.innerText = "";
-            errorDiv.classList.remove("error-message");
-        }, 5000);
-
-        return false; // Evita el envío del formulario si hay campos vacíos
+    // Código anterior para el mensaje de registro exitoso...
+  }
+  
+  function validateForm() {
+    const name = document.getElementById("inputName").value.trim();
+    const lastName = document.getElementById("inputLastName").value.trim();
+    const phone = document.getElementById("inputPhone").value.trim();
+    const email = document.getElementById("inputEmail").value.trim();
+    const password = document.getElementById("inputPassword").value;
+  
+    const errorDiv = document.getElementById("errorMessage");
+  
+    if (name === "" || lastName === "" || phone === "" || email === "" || password === "") {
+      // Código anterior para campos vacíos...
     }
-
-    // Todos los campos están llenos, mostrar mensaje de registro exitoso
+  
+    if (!validateEmail(email)) {
+      errorDiv.innerText = "Por favor, introduzca un correo electrónico válido.";
+      errorDiv.classList.add("error-message");
+  
+      // Eliminar el mensaje de error después de 5 segundos
+      setTimeout(function () {
+        errorDiv.innerText = "";
+        errorDiv.classList.remove("error-message");
+      }, 5000);
+  
+      return false; // Evita el envío del formulario si el correo electrónico no es válido
+    }
+  
+    if (!validatePhoneNumber(phone)) {
+      errorDiv.innerText = "Por favor, introduzca un número de teléfono válido (10 dígitos).";
+      errorDiv.classList.add("error-message");
+  
+      // Eliminar el mensaje de error después de 5 segundos
+      setTimeout(function () {
+        errorDiv.innerText = "";
+        errorDiv.classList.remove("error-message");
+      }, 5000);
+  
+      return false; // Evita el envío del formulario si el número de teléfono no es válido
+    }
+  
+    if (!validateName(name) || !validateName(lastName)) {
+      errorDiv.innerText = "Nombre y apellido no pueden contener símbolos.";
+      errorDiv.classList.add("error-message");
+  
+      // Eliminar el mensaje de error después de 5 segundos
+      setTimeout(function () {
+        errorDiv.innerText = "";
+        errorDiv.classList.remove("error-message");
+      }, 5000);
+  
+      return false; // Evita el envío del formulario si el nombre o apellido contienen símbolos
+    }
+  
+    if (password.length < 8) {
+      errorDiv.innerText = "La contraseña debe tener al menos 8 caracteres.";
+      errorDiv.classList.add("error-message");
+  
+      // Eliminar el mensaje de error después de 5 segundos
+      setTimeout(function () {
+        errorDiv.innerText = "";
+        errorDiv.classList.remove("error-message");
+      }, 5000);
+  
+      return false; // Evita el envío del formulario si la contraseña es demasiado corta
+    }
+  
+    // Si todo está bien, mostrar mensaje de registro exitoso
     registerSuccessMessage();
-
     return true;
-}
-
-function userRegister(){
-    let name = document.getElementById("inputName").value;
-    let lastName = document.getElementById("inputLastName").value;
-    let phone = document.getElementById("inputPhone").value;
-    let email = document.getElementById("inputEmail").value;
-    let password = document.getElementById("inputPassword").value;
-
-    console.log(name, lastName, phone, email, password);
-
-    const userData = {
-        nombre: name,
-        apellido: lastName,
-        telefono: phone,
-        email: email,
-        contraseña: password
-    }
-
-    const url = "http://localhost:8080/user";
-
-    const requestOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(userData)
-    }
-
-    fetch(url, requestOptions)
-        .then((response) => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error('Error al registrar usuario : ');
-            }
-        })
-        .then((data) => {
-            console.log('Usuario registrado con éxito', data);
-        })
-        .catch((error) => {
-            console.log('Error al realizar el registro', error);
-        });
-}
+  }
+  
+  function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+  
+  function validatePhoneNumber(phone) {
+    const phoneRegex = /^\d{10}$/;
+    return phoneRegex.test(phone);
+  }
+  
+  function validateName(name) {
+    const symbolsRegex = /[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/;
+    return !symbolsRegex.test(name);
+  }
+  
+  function userRegister() {
+    // Código anterior para registro de usuario...
+  }
+  
